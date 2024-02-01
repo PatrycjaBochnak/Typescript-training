3ef1ded7ddb495d5030707a1ed39a0615fa0b110
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-02-01 22:58:28 +0100

    classes

--- 5./dist/app.js
+++ 5./dist/app.js
@@ -1,8 +1,25 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    describe() {
+        console.log('Deparment: ' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 const accounting = new Department('Accounting');
-console.log(accounting);
+accounting.addEmployee('Max');
+accounting.addEmployee('Anna');
+// accounting.employees[2] = 'Tom'; // it works only with simple classes
+accounting.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'Pat', describe: accounting.describe } 
+// accountingCopy.describe();
--- 5./src/app.ts
+++ 5./src/app.ts
@@ -1,11 +1,33 @@
 class Department {
    name: string;
+   private employees:string[] = [];
 
    constructor(n: string){
     this.name = n;
+   }
+
+   describe(this: Department) {
+    console.log('Deparment: ' + this.name)
+   }
+   addEmployee(employee: string) {
+    this.employees.push(employee);
+   }
+   printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees);
    }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting)
+accounting.addEmployee('Max');
+accounting.addEmployee('Anna');
+
+// accounting.employees[2] = 'Tom'; // it works only with simple classes
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'Pat', describe: accounting.describe } 
+
+// accountingCopy.describe();
