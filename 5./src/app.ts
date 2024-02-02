737b1e53cd2e0a62b078736df779e0d76fedefd6
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-02-02 12:13:17 +0100

    public private

--- 5./src/app.ts
+++ 5./src/app.ts
@@ -1,33 +1,35 @@
 class Department {
-   name: string;
-   private employees:string[] = [];
+//   private id: string;
+//   private name: string;
+  private employees: string[] = [];
 
-   constructor(n: string){
-    this.name = n;
-   }
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
 
-   describe(this: Department) {
-    console.log('Deparment: ' + this.name)
-   }
-   addEmployee(employee: string) {
+  describe(this: Department) {
+    console.log(`Department (${this.id}) :${this.name}`);
+  }
+  addEmployee(employee: string) {
     this.employees.push(employee);
-   }
-   printEmployeeInformation() {
-    console.log(this.employees.length)
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
     console.log(this.employees);
-   }
+  }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department("d1", "Accounting");
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Anna');
+accounting.addEmployee("Max");
+accounting.addEmployee("Anna");
 
 // accounting.employees[2] = 'Tom'; // it works only with simple classes
 
 accounting.describe();
 accounting.printEmployeeInformation();
 
-// const accountingCopy = { name: 'Pat', describe: accounting.describe } 
+// const accountingCopy = { name: 'Pat', describe: accounting.describe }
 
 // accountingCopy.describe();
