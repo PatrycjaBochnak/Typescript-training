18b96701b4a126000cc2ef2e121c38218a9955af
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-02-07 19:35:50 +0100

    protected

--- 5./src/app.ts
+++ 5./src/app.ts
@@ -1,7 +1,7 @@
 class Department {
 //   private readonly id: string;
 //   private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -30,6 +30,12 @@ class AccountingDepartment extends Department {
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting')
     }
+    addEmployee(name: string) {
+        if ( name === 'Max') {
+            return;
+        }
+        this.employees.push(name);
+    }
     addReport(text: string) {
         this.reports.push(text);
     }
@@ -53,7 +59,12 @@ console.log(it);
 const accounting = new AccountingDepartment('d2', [])
 
 accounting.addReport('Something went wrong');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu')
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 // const itCopy = { name: 'Pat', describe: it.describe }
 
 // itCopy.describe();
