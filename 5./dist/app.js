b24d3a685639f700e810d7b293533be9b847a057
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-02-08 19:11:50 +0100

    classes

--- 5./dist/app.js
+++ 5./dist/app.js
@@ -9,6 +9,9 @@ class Department {
         // this.id = id;
         // this.name = n;
     }
+    static createEmployee(name) {
+        return { name: name };
+    }
     describe() {
         console.log(`Department (${this.id}) :${this.name}`);
     }
@@ -27,9 +30,22 @@ class ITDepartment extends Department {
     }
 }
 class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found');
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error('Please pass in a valid value');
+        }
+        this.addReport(value);
+    }
     constructor(id, reports) {
         super(id, 'Accounting');
         this.reports = reports;
+        this.lastReport = reports[0];
     }
     addEmployee(name) {
         if (name === 'Max') {
@@ -39,11 +55,14 @@ class AccountingDepartment extends Department {
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
     }
 }
+const employee1 = Department.createEmployee('Zdzichu');
+console.log(employee1);
 const it = new ITDepartment("d1", ['Pati']);
 it.addEmployee("Max");
 it.addEmployee("Anna");
@@ -53,7 +72,9 @@ it.name = 'NEW NAME';
 it.printEmployeeInformation();
 console.log(it);
 const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Year end report';
 accounting.addReport('Something went wrong');
+console.log(accounting.mostRecentReport);
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 accounting.printReports();
